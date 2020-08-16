7844f478d1b79eb4e3ee27de6908ba2086f22695
moly7x <16718453+moly7x@users.noreply.github.com>
2020-08-16 16:01:18 +0700

    update 16/08

--- Java/Workspace Java/taco-cloud/src/test/java/tacos/TacoCloudApplicationTests.java
+++ Java/Workspace Java/taco-cloud/src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,10 +1,10 @@
-package com.moly7x.first;
+package tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FirstprojApplicationTests {
+class TacoCloudApplicationTests {
 
 	@Test
 	void contextLoads() {
