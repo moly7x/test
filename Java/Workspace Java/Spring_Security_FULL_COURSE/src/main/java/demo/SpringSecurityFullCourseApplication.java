49b94daa99e880b7333fbe96ad2b8c1ed9cdfec7
moly7x <16718453+moly7x@users.noreply.github.com>
2020-09-06 15:03:41 +0700

    6/9

---
+++ Java/Workspace Java/Spring_Security_FULL_COURSE/src/main/java/demo/SpringSecurityFullCourseApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFullCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFullCourseApplication.class, args);
+	}
+
+}
